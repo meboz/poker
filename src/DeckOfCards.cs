960eebd4be25ec62c2f7b80194db587f909d3b57
Byron Walker <byron.walker@angryagile.com.au>
2012-07-24 15:26:56 +1000

    Deck can deal a hand, need to test for randomness

--- src/DeckOfCards.cs
+++ src/DeckOfCards.cs
@@ -1,9 +1,15 @@
 using System;
 using System.Collections.Generic;
 using poker.tests;
+using System.Linq;
 
 namespace poker
 {
+	public class NeedToRefillTheShowException : Exception
+	{
+		
+	}
+
 	public class DeckOfCards
 	{
 		public List<Card> Cards { get; set; }
@@ -18,6 +24,23 @@ namespace poker
 					Cards.Add(new Card((Suit)value, i));
 			}
 			
+		}
+
+		public Card[] DealHand(int i)
+		{
+			var hand = new List<Card>();
+			if(Cards.Count < i)
+				throw new NeedToRefillTheShowException();
+
+
+			for (var j = 0; j < i; j++)
+			{
+				var cardIndexToPick = new Random().Next(0, Cards.Count);
+				hand.Add(Cards.ElementAt(cardIndexToPick));
+				Cards.RemoveAt(cardIndexToPick);
+			}
+
+			return hand.ToArray();
 		}
 	}
 }
--- tests/DeckTests.cs
+++ tests/DeckTests.cs
@@ -18,5 +18,36 @@ namespace poker.tests
 			deck.Cards[26].Suit.ShouldEqual(Suit.Clubs);
 			deck.Cards[39].Suit.ShouldEqual(Suit.Spades);
 		}
+
+		[Test]
+		[ExpectedException(typeof(NeedToRefillTheShowException))]
+		public void should_throw_if_deck_doesnt_have_enough_cards()
+		{
+			var deck = new DeckOfCards();
+			var hand = deck.DealHand(80);
+		}
+
+		[Test]
+		public void should_get_the_right_number_of_cards_youve_requested()
+		{
+			var deck = new DeckOfCards();
+			var hand = deck.DealHand(5);
+
+			hand.Length.ShouldEqual(5);
+		}
+
+		[Test]
+		public void should_remove_cards_from_deck_after_hand_is_dealt()
+		{
+			var deck = new DeckOfCards();
+			var hand = deck.DealHand(5);
+
+			deck.Cards.Count.ShouldEqual(47);
+
+			var anotherHand = deck.DealHand(5);
+
+			deck.Cards.Count.ShouldEqual(42);
+		}
+
 	}
 }
